81d2cbf6af126b1ef8694d1c4ce39b2ce505ae83
leylaheydarova <leylaheydarova.dev@gmail.com>
2026-02-27 00:28:18 +0400

    Front to back is done

---
+++ Portfolio.Web/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio.Web/Controllers/PortfolioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Web.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
